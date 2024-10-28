1edd70f1c6fc6f5d21549d7a9e8c0e34ef6f35c8
xkodxdf <sirota92@bk.ru>
2024-10-28 13:57:54 +0300

    align: with |Lesson08 DateTime api| commit

---
+++ src/com/xkodxdf/webapp/MainDate.java
@@ -0,0 +1,39 @@
+package com.xkodxdf.webapp;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class MainDate {
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        Date date = new Date();
+        System.out.println(date);
+        System.out.println(System.currentTimeMillis() - start);
+        System.out.println("_".repeat(80));
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
+        System.out.println(calendar.getTime());
+        System.out.println("_".repeat(80));
+
+        LocalDate ld = LocalDate.now();
+        LocalTime lt = LocalTime.now();
+        LocalDateTime ldt = LocalDateTime.of(ld, lt);
+        System.out.println(ldt);
+        System.out.println("_".repeat(80));
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yy/MM/dd");
+        System.out.println(sdf.format(date));
+        System.out.println("_".repeat(80));
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yy/MM/dd");
+        System.out.println(dtf.format(ldt));
+    }
+}
