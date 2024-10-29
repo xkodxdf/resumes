87adc8840d78f79c220a4de5b72ceb0f400b5ec2
xkodxdf <sirota92@bk.ru>
2024-10-29 14:51:42 +0300

    add: printProjectStructure()

--- src/com/xkodxdf/webapp/MainFile.java
+++ src/com/xkodxdf/webapp/MainFile.java
@@ -3,12 +3,17 @@ package com.xkodxdf.webapp;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Objects;
 
 public class MainFile {
 
     public static void main(String[] args) {
-        String filePath = ".\\.gitignore";
+        File baseDir = new File(System.getProperty("user.dir"));
+        printProjectStructure(baseDir);
+        System.out.println("#".repeat(80));
 
+
+        String filePath = ".\\.gitignore";
         File file = new File(filePath);
         try {
             System.out.println(file.getCanonicalPath());
@@ -29,6 +34,19 @@ public class MainFile {
             System.out.println(fis.read());
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static void printProjectStructure(File root) {
+        File[] files = root.listFiles();
+        if (Objects.nonNull(files)) {
+            for (File file : files) {
+                String[] path = file.getParent().split("\\\\");
+                System.out.println(file.isDirectory() ?
+                        "\nDIRECTORY:" + file.getName().toUpperCase()
+                        : "../" + path[path.length - 1] + "/" + file.getName());
+                printProjectStructure(file);
+            }
         }
     }
 }
