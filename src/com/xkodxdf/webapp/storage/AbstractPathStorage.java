6a131e4d56ab5ab0ef7aca6b327ab94f4aff1a3c
xkodxdf <sirota92@bk.ru>
2024-11-04 17:19:07 +0300

    hw9[implement ObjectStreamPathStorage and add ObjectStreamPathStorageTest]

--- src/com/xkodxdf/webapp/storage/AbstractPathStorage.java
+++ src/com/xkodxdf/webapp/storage/AbstractPathStorage.java
@@ -3,13 +3,12 @@ package com.xkodxdf.webapp.storage;
 import com.xkodxdf.webapp.exception.StorageException;
 import com.xkodxdf.webapp.model.Resume;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public abstract class AbstractPathStorage extends AbstractStorage<Path> {
@@ -29,7 +28,11 @@ public abstract class AbstractPathStorage extends AbstractStorage<Path> {
 
     @Override
     public int size() {
-        return 0;
+        try (Stream<Path> files = Files.list(directory)) {
+            return (int) files.count();
+        } catch (IOException e) {
+            throw new StorageException(directory.getFileName() + ":directory error", "");
+        }
     }
 
     @Override
@@ -43,37 +46,58 @@ public abstract class AbstractPathStorage extends AbstractStorage<Path> {
 
     @Override
     protected boolean isExist(Path path) {
-        return false;
+        return Files.exists(path);
     }
 
     @Override
     protected Path getSearchKey(String uuid) {
-        return null;
+        return directory.resolve(uuid);
     }
 
     @Override
     protected void doSave(Resume r, Path path) {
-
+        try {
+            Files.createFile(path);
+        } catch (IOException e) {
+            throw new StorageException("IO error", path.getFileName().toString(), e);
+        }
+        doUpdate(r, path);
     }
 
     @Override
     protected Resume doGet(Path path) {
-        return null;
+        try {
+            return doRead(new BufferedInputStream(new FileInputStream(path.toFile())));
+        } catch (IOException e) {
+            throw new StorageException("IO error", path.getFileName().toString(), e);
+        }
     }
 
     @Override
     protected void doUpdate(Resume r, Path path) {
-
+        try {
+            doWrite(r, new BufferedOutputStream(new FileOutputStream(path.toFile())));
+        } catch (IOException e) {
+            throw new StorageException("IO error", path.getFileName().toString());
+        }
     }
 
     @Override
     protected void doDelete(Path path) {
-
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            throw new StorageException("File delete error", path.getFileName().toString(), e);
+        }
     }
 
     @Override
     protected List<Resume> doCopy() {
-        return List.of();
+        try (Stream<Path> files = Files.list(directory)) {
+            return files.map(this::doGet).collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new StorageException(directory.getFileName().toString() + ":directory error", "");
+        }
     }
 
     protected abstract Resume doRead(InputStream is) throws IOException;
