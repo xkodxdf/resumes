08794c7c86c1ce7a3980cbded99299e3e81f32ed
xkodxdf <sirota92@bk.ru>
2024-12-06 19:08:01 +0300

    align: with |Lesson15 servlets| commit

---
+++ src/com/xkodxdf/webapp/web/ResumeServlet.java
@@ -0,0 +1,21 @@
+package com.xkodxdf.webapp.web;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ResumeServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+//        response.setHeader("Content-Type", "text/html; charset=UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        String name = request.getParameter("name");
+        response.getWriter().write(name == null ? "Hello Resumes!" : "<h1>Hello " + name + "!</h1>");
+    }
+}
