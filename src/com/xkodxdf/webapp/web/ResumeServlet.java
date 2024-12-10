63c2c1b264ed9c092f6045100ca937eff73a782d
xkodxdf <sirota92@bk.ru>
2024-12-10 13:19:45 +0300

    hw15[make table display in the servlet (resume table, i.e. only uuid and fullName]

--- src/com/xkodxdf/webapp/web/ResumeServlet.java
+++ src/com/xkodxdf/webapp/web/ResumeServlet.java
@@ -1,11 +1,18 @@
 package com.xkodxdf.webapp.web;
 
+import com.xkodxdf.webapp.Config;
+import com.xkodxdf.webapp.model.Resume;
+import com.xkodxdf.webapp.storage.Storage;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
+
+    private final Storage sqlStorage = Config.get().getSqlStorage();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
     }
@@ -15,7 +22,22 @@ public class ResumeServlet extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
 //        response.setHeader("Content-Type", "text/html; charset=UTF-8");
         response.setContentType("text/html; charset=UTF-8");
-        String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello Resumes!" : "<h1>Hello " + name + "!</h1>");
+//        String name = request.getParameter("name");
+//        response.getWriter().write(name == null ? "Hello Resumes!" : "<h1>Hello " + name + "!</h1>");
+        Writer writer = response.getWriter();
+        writer.write(
+                "<table border='3'>" +
+                        "<tr>" +
+                        "<th>uuid</th>" +
+                        "<th>full_name</th>" +
+                        "</tr>");
+        for (Resume r : sqlStorage.getAllSorted()) {
+            writer.write(
+                    "<tr>" +
+                            "<td>" + r.getUuid() + "</td>" +
+                            "<td>" + r.getFullName() + "</td>" +
+                            "</tr>");
+        }
+        writer.write("</table>");
     }
 }
