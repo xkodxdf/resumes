a9bd83ca3e457bf5bf49b9dcf7701dc65924515d
xkodxdf <sirota92@bk.ru>
2024-12-18 11:12:26 +0300

    hw16[ref:naming]

--- src/com/xkodxdf/webapp/model/SectionType.java
+++ src/com/xkodxdf/webapp/model/SectionType.java
@@ -9,13 +9,13 @@ public enum SectionType {
     EXPERIENCE("Опыт работы"),
     EDUCATION("Образование");
 
-    private final String type;
+    private final String title;
 
-    SectionType(String type) {
-        this.type = type;
+    SectionType(String title) {
+        this.title = title;
     }
 
-    public String getType() {
-        return type;
+    public String getTitle() {
+        return title;
     }
 }
