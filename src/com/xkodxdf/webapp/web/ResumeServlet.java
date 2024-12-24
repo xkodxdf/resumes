31d8dff6c302f6f05c638d742734a3fae022945b
xkodxdf <sirota92@bk.ru>
2024-12-24 13:04:10 +0300

    hw16[ref:remove an unused exception]

--- src/com/xkodxdf/webapp/web/ResumeServlet.java
+++ src/com/xkodxdf/webapp/web/ResumeServlet.java
@@ -17,7 +17,7 @@ public class ResumeServlet extends HttpServlet {
 
     private final Storage storage = Config.get().getSqlStorage();
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         request.setCharacterEncoding("UTF-8");
         String uuid = request.getParameter("uuid");
         String fullName = request.getParameter("fullName");
